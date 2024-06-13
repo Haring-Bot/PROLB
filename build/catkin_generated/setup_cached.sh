df6ad7955c889f04bda653a58c5857bca991ea2f
JulianHaring <haringjulian@gmail.com>
2024-06-13 09:33:12 +0000

    init

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/fhtw_user/catkin_ws/src/PROLB/build/devel:$CMAKE_PREFIX_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/fhtw_user/catkin_ws/src/PROLB/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/fhtw_user/catkin_ws/src/PROLB:$ROS_PACKAGE_PATH"
