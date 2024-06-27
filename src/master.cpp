c9a3fadc33fa36c960e17f52a0bdd1f21476b200
JulianHaring <haringjulian@gmail.com>
2024-06-27 20:41:02 +0000

    expanded framework

--- src/master.cpp
+++ src/master.cpp
@@ -1,46 +1,78 @@
+#include <iostream>
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
 
-void callbackRcv(const std_msgs::String::ConstPtr& msg){
-    ROS_INFO("I received: [%s]", msg->data.c_str());
-}
+class controlRobot {
+public:
+    controlRobot(ros::NodeHandle& nh) : nh_(nh) {
+        // Additional initialization for controlRobot if needed
+    }
 
-void talkToMe(int argc, char **argv, std::string msg){
-    ros::init(argc, argv, "publisherTest");
-    ros::NodeHandle n;
-    ros::Publisher testPub = n.advertise<std_msgs::String>("testSubject", 1000);
-    ros::Rate loop_rate(10);
-    int i = 0;
-    while(ros::ok()){
+    void controller(float cmdVel, float cmdAng) {
+        geometry_msgs::Twist moveCmd;
+        moveCmd.linear.x = cmdVel;
+        moveCmd.angular.z = cmdAng;
+
+        // Assuming robotController is a member of controlRobot
+        robotController.publish(moveCmd);
+    }
+
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher robotController;  // Define your publisher here
+};
+
+class testMsgs {
+public:
+    testMsgs(ros::NodeHandle& nh) : nh_(nh) {
+        testPub = nh_.advertise<std_msgs::String>("testSubject", 1000);
+        testSub = nh_.subscribe("testSubject", 1000, &testMsgs::callbackRcv, this);
+    }
+
+    void callbackRcv(const std_msgs::String::ConstPtr& msg) {
+        ROS_INFO("I received: [%s]", msg->data.c_str());
+    }
+
+    void publisher(std::string msg) {
         std_msgs::String pblMsg;
         std::stringstream ss;
         ss << msg << i;
         pblMsg.data = ss.str();
         ROS_INFO("%s", pblMsg.data.c_str());
-        testPub.publish(pblMsg);  // Publish std_msgs::String, not std::string
-        ros::spinOnce();
-        loop_rate.sleep();
+        testPub.publish(pblMsg);
         i++;
     }
-}
 
-std_msgs::String listenToMe(int argc, char **argv){
-    std_msgs::String msg;
-    ros::init(argc, argv, "subscriberTest");
-    ros::NodeHandle n;
-    ros::Subscriber testSub = n.subscribe("testSubject", 1000, callbackRcv);
-    //msg = msg;
-    ros::spin();
+    std_msgs::String subscriber(ros::Rate freq) {
+        ros::spinOnce();  // Typically you would spin to handle callbacks
+        freq.sleep();
+        return receivedMsg;  // Return the received message
+    }
 
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher testPub;
+    ros::Subscriber testSub;
+    std_msgs::String receivedMsg;  // Store received message
+    int i = 0;
+};
 
-    //return msg;
-}
-
-
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     std::cout << "starting up\n";
-    talkToMe(argc, argv, "This is a test message");
-    listenToMe(argc, argv);                                                                                        //Single-threaded spinning
+    ros::init(argc, argv, "my_node");  // Initialize ROS
+    ros::NodeHandle nh;
+
+    testMsgs myTest(nh);
+
+    while(ros::ok()){
+        myTest.publisher("test");
+        std_msgs::String received = myTest.subscriber(10);
+        ROS_INFO("Received message: %s", received.data.c_str());
+        ros::spinOnce();  // Enter ROS event loop  
+    }
+
+
     return 0;
 }
-
