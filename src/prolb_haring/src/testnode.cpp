56d52ce2bdbc43d8f0d6de039c14969561d46b50
Julian Haring <haringjulian@gmail.com>
2025-07-01 11:59:05 +0200

    finalised KF

--- src/prolb_haring/src/testnode.cpp
+++ src/prolb_haring/src/testnode.cpp
@@ -7,19 +7,25 @@
 #include "std_msgs/msg/float64.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "sensor_msgs/msg/imu.hpp"
-#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 class KF:public rclcpp::Node{
   public:
     double interval = 0.02;
 
+
     KF():Node("kalman_node"){
+      mu << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+      sigma = Eigen::Matrix<double, 6, 6>::Identity();
+
       odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
         "/odom", 
         10,
         std::bind(&KF::odomCallback, this, std::placeholders::_1));
     
-      cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
         "/cmd_vel",
         10,
         std::bind(&KF::cmd_velCallback, this, std::placeholders::_1));
@@ -36,25 +42,23 @@ class KF:public rclcpp::Node{
     }
   private:
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr cmd_vel_sub_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
 
     rclcpp::TimerBase::SharedPtr timer_;
 
     std::unique_ptr<nav_msgs::msg::Odometry> latestOdom;
-    std::unique_ptr<geometry_msgs::msg::Twist> latestCmd_vel;
+    std::unique_ptr<geometry_msgs::msg::TwistStamped> latestCmd_vel;
     std::unique_ptr<sensor_msgs::msg::Imu> latestImu;
-    
-    Eigen::Matrix<double, 6, 1> muPrev;
     
     void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
       latestOdom = std::make_unique<nav_msgs::msg::Odometry>(*msg);
     }
 
-    void cmd_velCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
+    void cmd_velCallback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
     {
-      latestCmd_vel = std::make_unique<geometry_msgs::msg::Twist>(*msg);
+      latestCmd_vel = std::make_unique<geometry_msgs::msg::TwistStamped>(*msg);
     }
 
     void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg)
@@ -69,16 +73,40 @@ class KF:public rclcpp::Node{
         RCLCPP_INFO(this->get_logger(), "Waiting for all data...");
         return;
       }
+      Eigen::Matrix<double, 2, 1> u;
+      u(0) = latestCmd_vel->twist.linear.x;
+      u(1) = latestCmd_vel->twist.angular.z;
+
+      Eigen::Matrix<double, 6, 1> z;
+      z(0) = latestOdom->pose.pose.position.x;
+      z(1) = latestOdom->pose.pose.position.y;
+      z(2) = quaternionToYaw(latestOdom->pose.pose.orientation);
+      z(3) = latestOdom->twist.twist.linear.x;
+      z(4) = latestOdom->twist.twist.linear.y;
+      z(5) = latestOdom->twist.twist.angular.z;
+
+      calculateKalman(mu, sigma, u, z);
+
+      RCLCPP_INFO(this->get_logger(), "Estimated Pose -> x: %.3f, y: %.3f, theta: %.3f", mu(0), mu(1), mu(2));
+      RCLCPP_INFO(this->get_logger(), "Simplified uncertainty (trace of covariance): %.6f", sigma.trace());
+
     }
 
-    void calculateKalman(Eigen::Matrix<double, 6, 1> mu, Eigen::Matrix<double, 6, 6> sigma, Eigen::Matrix<double, 2, 1> u, Eigen::Matrix<double, 2, 1> z)
+    // struct kalmanReturn{
+    //   Eigen::Matrix<double, 6, 1> mu;
+    //   Eigen::Matrix<double, 6, 6> sigma;
+    // };
+    Eigen::Matrix<double, 6, 1> mu;
+    Eigen::Matrix<double, 6, 6> sigma;
+
+    void calculateKalman(const Eigen::Matrix<double, 2, 1> u, const Eigen::Matrix<double, 6, 1> z)
     {
-      double x = mu(0);
-      double y = mu(1);
-      double theta = mu(2);
-      double x_vel = mu(3);
-      double y_vel = mu(4);
-      double theta_vel = mu(5);
+      double x = this->mu(0);
+      double y = this->mu(1);
+      double theta = this->mu(2);
+      double x_vel = this->mu(3);
+      double y_vel = this->mu(4);
+      double theta_vel = this->mu(5);
 
       Eigen::Matrix<double, 6, 6> A = Eigen::Matrix<double, 6, 6>::Identity();
       A(0,3) = interval;
@@ -98,19 +126,29 @@ class KF:public rclcpp::Node{
 
       Eigen::Matrix<double, 6, 1> muPred = A * mu + B * u;    //apply motion model
 
-      Eigen::Matrix2d R;  //Cov linear model
-      R.setZero();
-      R(0,0) = 0.01;
-      R(1,1) = 0.01;
+      Eigen::Matrix<double, 6, 6> R = 0.1 * Eigen::MatrixXd::Identity(6,6); // Cov linear model
 
-      Eigen::Matrix<double, 6, 6> sigmaPred = A * sigma * A.transpose() + R;    //predicted covariance
+      Eigen::Matrix<double, 6, 6> sigmaPred = A * this->sigma * A.transpose() + R;    //predicted covariance
 
-      Eigen::Matrix<double, 6, 6> C = (1.0/interval) * Eigen::MatrixXd::Identity(6,6);
+      Eigen::Matrix<double, 6, 6> C = Eigen::MatrixXd::Identity(6,6);
       Eigen::Matrix<double, 6, 6> Q = 0.1 * Eigen::MatrixXd::Identity(6,6);
       Eigen::Matrix<double, 6, 6> kalmanGain = sigmaPred * C.transpose() * (C * sigmaPred * C.transpose() + Q).inverse();     //calculate Kalman Gain
 
       Eigen::Matrix<double, 6, 1> muCor = muPred + kalmanGain * (z - C * muPred);       //correct prediction based on Kalman Gain
       Eigen::Matrix<double, 6, 6> sigmaCor = (Eigen::MatrixXd::Identity(6,6) - kalmanGain * C) * sigmaPred;    //correct covariance based on Kalman Gain
+    
+      this->mu = muCor;
+      this->sigma = sigmaCor;
+
+      return;
+    }
+
+    double quaternionToYaw(const geometry_msgs::msg::Quaternion &q)
+    {
+      tf2::Quaternion tf_q(q.x, q.y, q.z, q.w);
+      double roll, pitch, yaw;
+      tf2::Matrix3x3(tf_q).getRPY(roll, pitch, yaw);
+      return yaw;
     }
 
 };
